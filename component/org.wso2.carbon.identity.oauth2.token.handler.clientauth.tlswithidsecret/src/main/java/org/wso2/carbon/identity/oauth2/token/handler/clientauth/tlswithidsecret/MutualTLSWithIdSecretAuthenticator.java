931c3366a380dc203dbe2e07187fb2f8e5a0b13b
nuwandiw <nuwandiw@wso2.com>
2018-10-04 14:15:32 +0530

    Adding license header

--- component/org.wso2.carbon.identity.oauth2.token.handler.clientauth.tlswithidsecret/src/main/java/org/wso2/carbon/identity/oauth2/token/handler/clientauth/tlswithidsecret/MutualTLSWithIdSecretAuthenticator.java
+++ component/org.wso2.carbon.identity.oauth2.token.handler.clientauth.tlswithidsecret/src/main/java/org/wso2/carbon/identity/oauth2/token/handler/clientauth/tlswithidsecret/MutualTLSWithIdSecretAuthenticator.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License
+ */
 package org.wso2.carbon.identity.oauth2.token.handler.clientauth.tlswithidsecret;
 
 import org.apache.commons.lang.StringUtils;
@@ -22,6 +39,11 @@ import java.util.Map;
 
 import static org.wso2.carbon.identity.oauth2.token.handler.clientauth.tlswithidsecret.util.MutualTLSUtil.JAVAX_SERVLET_REQUEST_CERTIFICATE;
 
+/**
+ * This class is an enhancement for BasicAuthClientAuthenticator which validates the client certificate
+ * in addition to the client id and secret authentication
+ *
+ */
 public class MutualTLSWithIdSecretAuthenticator extends BasicAuthClientAuthenticator {
 
     private static Log log = LogFactory.getLog(MutualTLSWithIdSecretAuthenticator.class);
